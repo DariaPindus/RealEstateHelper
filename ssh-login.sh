fefe5cdcccee07760aae5e095866e560003c9e23
DariaPindus <daria.pindus@gmail.com>
2021-01-13 12:43:24 +0100

    ci file: add ssh login script

--- ssh-login.sh
+++ ssh-login.sh
@@ -1,6 +1,8 @@
 mkdir -p ~/.ssh
 echo "$SSH_PRIVATE_KEY" | tr -d '\r' > ~/.ssh/id_rsa
-chmod 777 ~/.ssh/id_rsa
+echo "Set access to file"
+chmod 600 ~/.ssh/id_rsa
+echo "Access set"
 'which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )'
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
