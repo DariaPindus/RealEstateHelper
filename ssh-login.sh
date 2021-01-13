cfe902a3b00d7ea5aebd9dd38582656f5ad17714
DariaPindus <daria.pindus@gmail.com>
2021-01-13 12:52:02 +0100

    ci file: add ssh login script

--- ssh-login.sh
+++ ssh-login.sh
@@ -1,10 +1,5 @@
-mkdir -p ~/.ssh
-echo "$SSH_PRIVATE_KEY" | tr -d '\r' > ~/.ssh/id_rsa
-echo "Set access to file"
-chmod 600 ~/.ssh/id_rsa
-echo "Access set"
-'which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )'
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
-ssh-keyscan -H $EC2_HOST >> ~/.ssh/known_hosts
-echo "Executed ssh login script.."
+eval $(ssh-agent -s)
+echo $SSH_PRIVATE_KEY > deploy-key
+## Fix permissions on key file and .ssh folder
+chmod 700 deploy-key; mkdir -p ~/.ssh; chmod 700 ~/.ssh
+ssh-add -k deploy-key
