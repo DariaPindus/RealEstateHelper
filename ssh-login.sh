8fd914d052f101f5be5ae3a84f42240c6f6a04a2
DariaPindus <daria.pindus@gmail.com>
2021-01-13 16:31:31 +0100

    ci file: add ssh login script

--- ssh-login.sh
+++ ssh-login.sh
@@ -1,8 +1,7 @@
 mkdir -p ~/.ssh
 echo "$SSH_PRIVATE_KEY" | tr -d '\r' > ~/.ssh/id_rsa
 chmod 700 ~/.ssh/id_rsa
-#'which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )'
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
+ssh-keyscan -H 'gitlab.com' >> ~/.ssh/known_hosts
 ssh-keyscan -H $EC2_HOST >> ~/.ssh/known_hosts
-echo "Executed ssh login script.."
