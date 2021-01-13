dde7c688f243369924f09fef86bd0f4aba6941c3
DariaPindus <daria.pindus@gmail.com>
2021-01-13 13:58:39 +0100

    ci file: add ssh login script

--- ssh-login.sh
+++ ssh-login.sh
@@ -1,5 +1,6 @@
 eval $(ssh-agent -s)
 echo $SSH_PRIVATE_KEY > deploy-key
+echo $SSH_PRIVATE_KEY
 ## Fix permissions on key file and .ssh folder
 chmod 700 deploy-key; mkdir -p ~/.ssh; chmod 700 ~/.ssh
 ssh-add -k deploy-key
